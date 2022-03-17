123908413b00b4cad51a840c84a1fea2c6c8fd83
GUSTAVO BELO <gustavobelo123@gmail.com>
2022-03-17 13:56:25 -0300

    test: skip server test

--- test/server_test.dart
+++ test/server_test.dart
@@ -3,29 +3,31 @@ import 'package:test/test.dart';
 import 'package:test_process/test_process.dart';
 
 void main() {
-  final port = '3000';
-  final host = 'http://0.0.0.0:$port';
+  group("Server Test >", () {
+    final port = '3000';
+    final host = 'http://0.0.0.0:$port';
 
-  setUp(() async {
-    await TestProcess.start(
-      'dart',
-      ['run', 'bin/server.dart'],
-      environment: {'PORT': port},
-    );
-  });
+    setUp(() async {
+      await TestProcess.start(
+        'dart',
+        ['run', 'bin/server.dart'],
+        environment: {'PORT': port},
+      );
+    });
 
-  test('Root', () async {
-    final response = await get(Uri.parse(host + '/'));
-    expect(response.statusCode, 200);
-    expect(response.body, 'Simple Crud Dart API!\n');
-  });
+    test('Root', () async {
+      final response = await get(Uri.parse(host + '/'));
+      expect(response.statusCode, 200);
+      expect(response.body, 'Simple Crud Dart API!\n');
+    });
 
-  test('Users', () async {
-    final response = await get(Uri.parse(host + '/users'));
-    expect(response.statusCode, 200);
-  });
-  test('404', () async {
-    final response = await get(Uri.parse(host + '/foobar'));
-    expect(response.statusCode, 404);
-  });
+    test('Users', () async {
+      final response = await get(Uri.parse(host + '/users'));
+      expect(response.statusCode, 200);
+    });
+    test('404', () async {
+      final response = await get(Uri.parse(host + '/foobar'));
+      expect(response.statusCode, 404);
+    });
+  }, skip: "These tests make a Real Call");
 }
