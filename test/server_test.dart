ad57bd4b3bd96dea40515aa922d2ceaed34c4b3f
GUSTAVO BELO <gustavobelo123@gmail.com>
2022-03-17 20:11:08 -0300

    refactor: server test listen from dart-define

--- test/server_test.dart
+++ test/server_test.dart
@@ -3,31 +3,39 @@ import 'package:test/test.dart';
 import 'package:test_process/test_process.dart';
 
 void main() {
-  group("Server Test >", () {
-    final port = '3000';
-    final host = 'http://0.0.0.0:$port';
+  group(
+    "Server Test >",
+    () {
+      final port = '3000';
+      final host = 'http://0.0.0.0:$port';
 
-    setUp(() async {
-      await TestProcess.start(
-        'dart',
-        ['run', 'bin/server.dart'],
-        environment: {'PORT': port},
-      );
-    });
+      setUp(() async {
+        await TestProcess.start(
+          'dart',
+          [
+            'run',
+            "--define=IS_PROD=false --define=SERVER_PORT=$port "
+                "--define=HOST=$host",
+            'bin/server.dart',
+          ],
+        );
+      });
 
-    test('Root', () async {
-      final response = await get(Uri.parse(host + '/'));
-      expect(response.statusCode, 200);
-      expect(response.body, 'Simple Crud Dart API!\n');
-    });
+      test('Root', () async {
+        final response = await get(Uri.parse(host + '/'));
+        expect(response.statusCode, 200);
+        expect(response.body, 'Simple Crud Dart API!\n');
+      });
 
-    test('Users', () async {
-      final response = await get(Uri.parse(host + '/users'));
-      expect(response.statusCode, 200);
-    });
-    test('404', () async {
-      final response = await get(Uri.parse(host + '/foobar'));
-      expect(response.statusCode, 404);
-    });
-  }, skip: "These tests make a Real Call");
+      test('Users', () async {
+        final response = await get(Uri.parse(host + '/users'),
+            headers: {"Authorization": "Bearer test"});
+        expect(response.statusCode, 200);
+      });
+      test('404', () async {
+        final response = await get(Uri.parse(host + '/foobar'));
+        expect(response.statusCode, 404);
+      });
+    },
+  );
 }
